02d56b7152e463f496f8b042bd27804703c54935
Matthieu <mattannequin@gmail.com>
2023-04-17 11:19:12 +0200

    add not in counter argument

--- mesa/arguments/Argument.py
+++ mesa/arguments/Argument.py
@@ -37,7 +37,7 @@ class Argument :
         self.couple_values_list.append(CoupleValue(criterion_name, value))
         
 
-    def List_supporting_proposal(self, item, preferences) :
+    def List_supporting_proposal(self, preferences) :
         """ Generate a list of premisses which can be used to support an item
         : param item : Item - name of the item
         : return : list of all premisses PRO an item (sorted by order of importance
@@ -45,7 +45,7 @@ class Argument :
         """
         criterion_list = preferences.get_criterion_name_list()
         for criterion_name_1 in criterion_list:
-            value = item.get_value(preferences, criterion_name_1)
+            value = self.item.get_value(preferences, criterion_name_1)
             if value == Value.VERY_GOOD or value == Value.GOOD:
                 self.add_premiss_couple_values(criterion_name_1, value)
             for criterion_name_2 in criterion_list:
@@ -54,7 +54,7 @@ class Argument :
         list_of_supporting_proposal = self.comparison_list + self.couple_values_list
         return list_of_supporting_proposal
         
-    def List_attacking_proposal(self, item, preferences) :
+    def List_attacking_proposal(self, preferences) :
         """ Generate a list of premisses which can be used to attack an item
         : param item : Item - name of the item
         : return : list of all premisses CON an item ( sorted by order of importance
@@ -62,7 +62,7 @@ class Argument :
         """
         criterion_list = preferences.get_criterion_name_list()
         for criterion_name_1 in criterion_list:
-            value = item.get_value(preferences, criterion_name_1)
+            value = self.item.get_value(preferences, criterion_name_1)
             if value == Value.VERY_BAD or value == Value.BAD:
                 self.add_premiss_couple_values(criterion_name_1, value)
             for criterion_name_2 in criterion_list:
@@ -71,26 +71,26 @@ class Argument :
         list_of_attacking_proposal = self.comparison_list + self.couple_values_list
         return list_of_attacking_proposal  
 
-    def support_proposal(self, item, preferences):
+    def support_proposal(self, preferences):
         """
         Used when the agent receives " ASK_WHY " after having proposed an item
         : param item : str - name of the item which was proposed
         : return : string - the strongest supportive argument
         """
-        list_of_supporting_proposal = self.List_supporting_proposal(item, preferences)
+        list_of_supporting_proposal = self.List_supporting_proposal(preferences)
         support_proposal = random.choice(list_of_supporting_proposal)
         if support_proposal.type == 'CoupleValue':
             value = support_proposal.value
             criterion_name = support_proposal.criterion_name
-            argument = str(criterion_name) + ' = ' + str(value)
+            argument = str(item) + ', ' + str(criterion_name) + ' = ' + str(value)
         else:
             best_criterion_name = support_proposal.best_criterion_name 
             worst_criterion_name = support_proposal.worst_criterion_name 
-            argument = str(best_criterion_name) + ' > ' + str(worst_criterion_name)
+            argument = str(item) + ', ' + str(best_criterion_name) + ' > ' + str(worst_criterion_name)
         return argument
 
-    def get_list_of_counter_arguments(self, item, preferences, argument):
-        list_of_attacking_arguments = self.List_attacking_proposal(item, preferences)
+    def get_list_of_counter_arguments(self, preferences, argument):
+        list_of_attacking_arguments = self.List_attacking_proposal(preferences)
         list_of_counter_arguments = list()
         for attacking_argument in list_of_attacking_arguments:
             if attacking_argument.type == 'CoupleValue':
@@ -101,15 +101,15 @@ class Argument :
                     list_of_counter_arguments.append(attacking_argument)
         return list_of_counter_arguments
 
-    def get_counter_argument(self, item, preferences, argument):
-        list_of_counter_arguments = self.get_list_of_counter_arguments(item, preferences, argument)
+    def get_counter_argument(self, preferences, argument):
+        list_of_counter_arguments = self.get_list_of_counter_arguments(preferences, argument)
         counter_proposal = random.choice(list_of_counter_arguments)
         if counter_proposal.type == 'CoupleValue':
             value = counter_proposal.value
             criterion_name = counter_proposal.criterion_name
-            counter_argument = str(criterion_name) + ' = ' + str(value)
+            counter_argument = 'not ' + str(self.item) + ', ' + str(criterion_name) + ' = ' + str(value)
         else:
             best_criterion_name = counter_proposal.best_criterion_name 
             worst_criterion_name = counter_proposal.worst_criterion_name 
-            counter_argument = str(best_criterion_name) + ' > ' + str(worst_criterion_name)
+            counter_argument = 'not ' + str(self.item) + ', ' + str(best_criterion_name) + ' > ' + str(worst_criterion_name)
         return counter_argument
