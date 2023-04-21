d894b9bacc7b099ac3dfbf9353c543d1fdb3c939
Matthieu <mattannequin@gmail.com>
2023-04-21 10:41:45 +0200

    IMPROVE counter arg

--- mesa/arguments/Argument.py
+++ mesa/arguments/Argument.py
@@ -135,16 +135,16 @@ class Argument :
                     possible_counter_argument = str(criterion_name) + ' = ' + str(agent_value)
                     if (engine,possible_counter_argument) not in list_agent_arg:
                         possible_counter_arguments.append((engine,possible_counter_argument))
-            if argument.value.value < agent_value.value:
-                criterion_list = preferences.get_criterion_name_list()
-                for criterion_name_2 in criterion_list:
-                    print(criterion_name_2, preferences.is_preferred_criterion(criterion_name_2, criterion_name),self.item.get_value(preferences, criterion_name_2).value < agent_value.value)
-                    if preferences.is_preferred_criterion(criterion_name_2, criterion_name) and criterion_name_2 != criterion_name and self.item.get_value(preferences, criterion_name_2).value < agent_value.value :
-                        self.add_premiss_couple_values(criterion_name_2, self.item.get_value(preferences, criterion_name_2))
-                        self.add_premiss_comparison(criterion_name_2, criterion_name)
-                        possible_counter_argument = str(criterion_name_2) + ' = ' + str(self.item.get_value(preferences, criterion_name_2)) + ' and ' + str(criterion_name_2) + ' > ' + str(criterion_name)
-                        if (self.item, possible_counter_argument) not in list_agent_arg:
-                            possible_counter_arguments.append((self.item,possible_counter_argument))
+            #if argument.value.value =< agent_value.value:
+            criterion_list = preferences.get_criterion_name_list()
+            for criterion_name_2 in criterion_list:
+                print(criterion_name_2, preferences.is_preferred_criterion(criterion_name_2, criterion_name),self.item.get_value(preferences, criterion_name_2).value < agent_value.value)
+                if preferences.is_preferred_criterion(criterion_name_2, criterion_name) and criterion_name_2 != criterion_name and self.item.get_value(preferences, criterion_name_2).value < agent_value.value :
+                    self.add_premiss_couple_values(criterion_name_2, self.item.get_value(preferences, criterion_name_2))
+                    self.add_premiss_comparison(criterion_name_2, criterion_name)
+                    possible_counter_argument = str(criterion_name_2) + ' = ' + str(self.item.get_value(preferences, criterion_name_2)) + ' and ' + str(criterion_name_2) + ' > ' + str(criterion_name)
+                    if (self.item, possible_counter_argument) not in list_agent_arg:
+                        possible_counter_arguments.append((self.item,possible_counter_argument))
         else:
             best_criterion_name = argument.best_criterion_name
             worst_criterion_name = argument.worst_criterion_name
@@ -165,15 +165,15 @@ class Argument :
                     possible_pro_argument = str(criterion_name) + ' = ' + str(agent_value)
                     if (engine, possible_pro_argument) not in list_agent_arguments:
                         possible_pro_arguments.append((engine,possible_pro_argument))
-            if argument.value.value < agent_value.value:
-                criterion_list = preferences.get_criterion_name_list()
-                for criterion_name_2 in criterion_list:
-                    if preferences.is_preferred_criterion(criterion_name_2, criterion_name) and criterion_name_2 != criterion_name and self.item.get_value(preferences, criterion_name_2).value > agent_value.value :
-                        self.add_premiss_couple_values(criterion_name_2, self.item.get_value(preferences, criterion_name_2))
-                        self.add_premiss_comparison(criterion_name_2, criterion_name)
-                        possible_pro_argument = str(criterion_name_2) + ' = ' + str(self.item.get_value(preferences, criterion_name_2)) + ' and ' + str(criterion_name_2) + ' > ' + str(criterion_name)
-                        if (self.item,possible_pro_argument) not in list_agent_arguments:
-                            possible_pro_arguments.append((self.item,possible_pro_argument))
+            #if argument.value.value < agent_value.value:
+            criterion_list = preferences.get_criterion_name_list()
+            for criterion_name_2 in criterion_list:
+                if preferences.is_preferred_criterion(criterion_name_2, criterion_name) and criterion_name_2 != criterion_name and self.item.get_value(preferences, criterion_name_2).value > agent_value.value :
+                    self.add_premiss_couple_values(criterion_name_2, self.item.get_value(preferences, criterion_name_2))
+                    self.add_premiss_comparison(criterion_name_2, criterion_name)
+                    possible_pro_argument = str(criterion_name_2) + ' = ' + str(self.item.get_value(preferences, criterion_name_2)) + ' and ' + str(criterion_name_2) + ' > ' + str(criterion_name)
+                    if (self.item,possible_pro_argument) not in list_agent_arguments:
+                        possible_pro_arguments.append((self.item,possible_pro_argument))
         else:
             best_criterion_name = argument.best_criterion_name
             worst_criterion_name = argument.worst_criterion_name
